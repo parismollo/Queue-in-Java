b87abfad6a6249ea6e755314f84e735d5ab96995
Paris Mollo <parismollochristondis@gmail.com>
2021-11-25 11:10:56 +0100

    add display feature

--- File.java
+++ File.java
@@ -17,6 +17,11 @@ class File {
     private int taille; // taile max de la file
     private int[] file; // la file en forme de tableau
 
+    private static String greenColor = "\u001B[32m";
+    private static String redColor = "\u001B[31m";
+    private static String resetColor = "\u001B[0m";
+    private static String yellowColor = "\u001B[33m";
+
     public File(int taille) {
         this.taille = taille;
         this.head = this.queue = 0;
@@ -24,8 +29,13 @@ class File {
     }
 
     public static void main(String[] args) {
-        File f = new File(1);
+        File f = new File(4);
         f.put(1);
+        f.display();
+        f.put(4);
+        f.display();
+        f.put(2);
+        f.display();
     }
 
     public void put(int x) {
@@ -33,18 +43,38 @@ class File {
         1. Est-ce que la file est rempli?
         2. Sinon, ajouter x dans la queue.
        */
-
        if (!this.isFull()) {
            setNewQueue(x);
        } else {
            displayFailedMessage("Put operation failed... :(");
        }
-}
+    }
+
+    public boolean empty() {
+        return (this.head == this.queue) ? true : false;
+    }
+
+    public void display() {
+        if (this.empty()) {
+            displayWarningMessage("This queue is empty... :(");
+        } else {
+            for(int i = this.head; i<this.queue; i++) {
+                if (i == this.head) {
+                    System.out.print(greenColor+"HEAD "+resetColor);
+                }
+                System.out.print(this.file[i]+" ");
+            }
+            System.out.print(redColor+"QUEUE"+resetColor);
+        }
+        System.out.println();
+    }
+
+    private void displayWarningMessage(String string) {
+        System.out.println(yellowColor+string+resetColor);
+    }
 
     private void displayFailedMessage(String string) {
-        String colorRed = "\u001B[31m";
-        String resetColor = "\u001B[0m";
-        System.out.println(colorRed+"[LOG]: "+string+resetColor);
+        System.out.println(redColor+"[LOG]: "+string+resetColor);
     }
 
     public void setNewQueue(int x) {
