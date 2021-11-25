fa12fed1063e2ed967a4212dd8b3892fbf212c69
Paris Mollo <parismollochristondis@gmail.com>
2021-11-25 10:58:37 +0100

    add put method

--- File.java
+++ File.java
@@ -12,27 +12,47 @@ du premier élément du tableau.
 */
 
 class File {
-    private int head;
-    private int queue;
-    private int taille;
-    private int[] file;
-    
-    public int getHead() {
-        return head;
-    }
-    public int getTaille() {
-        return taille;
-    }
-    public void setTaille(int taille) {
+    private int head; // pos du premier element dans la file
+    private int queue; // pos du dernier element dans la file 
+    private int taille; // taile max de la file
+    private int[] file; // la file en forme de tableau
+
+    public File(int taille) {
         this.taille = taille;
+        this.head = this.queue = 0;
+        this.file = new int[taille];
     }
-    public int getQueue() {
-        return queue;
+
+    public static void main(String[] args) {
+        File f = new File(1);
+        f.put(1);
     }
-    public void setQueue(int queue) {
-        this.queue = queue;
+
+    public void put(int x) {
+       /*
+        1. Est-ce que la file est rempli?
+        2. Sinon, ajouter x dans la queue.
+       */
+
+       if (!this.isFull()) {
+           setNewQueue(x);
+       } else {
+           displayFailedMessage("Put operation failed... :(");
+       }
+}
+
+    private void displayFailedMessage(String string) {
+        String colorRed = "\u001B[31m";
+        String resetColor = "\u001B[0m";
+        System.out.println(colorRed+"[LOG]: "+string+resetColor);
     }
-    public void setHead(int head) {
-        this.head = head;
+
+    public void setNewQueue(int x) {
+        this.file[this.queue] = x;
+        this.queue++;
+    }
+
+    public boolean isFull() {
+        return (this.queue == this.taille) ? true : false; // Bug:  == capacity -1? 
     }
 }
